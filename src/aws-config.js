d95ff93c39ab9f0a89b4ab4a63d9ecc78d9a5120
Robert Clark <rsclark2@gmail.com>
2020-02-01 11:30:46 -0500

    Switch user pool configuration

--- src/aws-config.js
+++ src/aws-config.js
@@ -1,8 +1,8 @@
 export default {
   Auth: {
     region: 'us-east-1',
-    userPoolId: 'us-east-1_2CJkxHreF',
-    userPoolWebClientId: 'l1avbnqv4jth155no9hg8kjpp',
+    userPoolId: 'us-east-1_1MvT1AVmj',
+    userPoolWebClientId: '2g5b9d09n5aq5b4oon1cu91d4e',
     authenticationFlowType: 'USER_SRP_AUTH'
   }
 };
