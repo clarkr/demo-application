1a15fc7c2afc81ac2e2e0b37d1d2617659292b2f
Robert Clark <rsclark2@gmail.com>
2020-04-19 13:13:38 -0400

    Remove ID from fields

--- src/api/ItemModel.js
+++ src/api/ItemModel.js
@@ -4,7 +4,6 @@ class ItemModel extends ApiModel {
   static apiPath = 'items'
 
   static fields = [
-    { key: 'id', name: 'Id', editable: false },
     { key: 'title', name: 'Title', editable: true },
     { key: 'content', name: 'Content', editable: true }
   ];
